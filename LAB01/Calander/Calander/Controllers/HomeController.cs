1157e874d2d447e9b254421db6694a2cace6f176
Akos Palosi <Akos.Palosi@ibm.com>
2024-02-14 17:41:39 +0100

    added LAB01

---
+++ LAB01/Calander/Calander/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Calander.Viewmodels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calander.Controllers
+{
+ 
+    public class HomeController : Controller
+    {
+        static List<task> tasks = new List<task>();
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult process(string taskname,int timespan, int end)
+        {
+            task task = new task() { taskname=taskname,timespan=timespan,finishtime=end};
+            if(!tasks.Where( t => t.finishtime- t.timespan == end-timespan).Any())
+            {
+
+                tasks.Add(task);
+
+                return RedirectToAction("Calander");
+            }
+            else
+            {
+                return Content("There is another task in this timeframe!");
+            }
+
+
+        }
+		public IActionResult Calander()
+		{
+			return View(tasks.ToArray());
+		}
+	}
+}
---
+++ LAB01/Calander/Calander/Viewmodels/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calander.Viewmodels
+{
+	public class task
+	{
+        [Required]
+        public string taskname { get; set; }
+        [Required]
+        [Range(1, 24, ErrorMessage = "value can be between 1 and 24")]
+        public int timespan { get; set; }
+		[Required]
+        [Range(1, 24, ErrorMessage = "value can be between 1 and 24")]
+        public int finishtime { get; set; }
+	}
+}
